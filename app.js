d9bb0c7027e542b3ce1198a5f2862dea85396850
kacper21galantowicz@gmail.com <kacper21galant@gmail.com>
2019-09-21 16:15:05 +0200

    percentages

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ var budgetController = (function () {
             });
         },
 
-        getPercentages: function(){
+        getPercentages: function () {
             var allPercentages = data.allItems.exp.map(function (current) {
                 return current.getPercentage();
             });
@@ -149,7 +149,8 @@ var UIController = (function () {
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage'
     };
 
     return {
@@ -227,10 +228,28 @@ var UIController = (function () {
             document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
 
             if (obj.percentage > 0) {
-                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
             } else {
                 document.querySelector(DOMstrings.percentageLabel).textContent = '---';
             }
+        },
+
+        displayPercentages: function (percentages) {
+            var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+            var nodeListForEach = function (list, callback) {
+                for (var i = 0; i < list.length; i++) {
+                    callback(list[i], i);
+                }
+            };
+
+            nodeListForEach(fields, function (current, index) {
+                if (percentages[index] > 0) {
+                    current.textContent = percentages[index] + '%';
+                } else {
+                    current.textContent = '---';
+                }
+            })
         },
 
         getDOMstrings: function () {
@@ -274,7 +293,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         //read this
         var percentages = budgetCtrl.getPercentages();
         //update UI
-        console.log(percentages);
+        UICtrl.displayPercentages(percentages);
 
     }
 
